c394b4ef4093166b87718a7e0ab2eddb41dc0a73
Adeel Ahmad <devadeelahmad@gmail.com>
2023-09-07 04:10:35 -0700

    Feat: Sign Up

--- App.js
+++ App.js
@@ -1,15 +1,113 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Pressable } from 'react-native';
-import View1 from './components/View';
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  Button,
+  Text,
+  Image,
+  Pressable,
+  Alert,
+} from "react-native";
+
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js</Text>
-      <StatusBar style="auto" />
-      <Pressable style={styles.button} onPress={View1}>
-      <Text style={styles.text}>My Button</Text>
-    </Pressable>
+    <View style={{ flex: 1 }}>
+      <View
+        style={{
+          flex: 0.3,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Image
+          style={{ width: 100, height: 100 }}
+          source={require("./Logo.png")}
+        />
+        <Text style={{paddingTop:20, color:"#854efe", fontSize:20, fontWeight:"600"}}>DBIT Decentralized Mining</Text>
+      </View>
+
+      <View
+        style={{
+          flex: 0.5,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <TextInput
+          placeholder={"Email"}
+          style={{
+            width: 270,
+            height: 50,
+            backgroundColor: "white",
+            borderRadius: 12,
+            borderColor: "#9041fe",
+            paddingLeft: 10,
+            borderWidth: 1,
+          }}
+        />
+
+        <TextInput
+          placeholder={"User Name"}
+          style={{
+            width: 270,
+            height: 50,
+            backgroundColor: "white",
+            color: "white",
+            marginTop: 20,
+            borderRadius: 12,
+            borderColor: "#9041fe",
+            paddingLeft: 10,
+            borderWidth: 1,
+          }}
+        />
+        <TextInput
+          placeholder="Password"
+          style={{
+            width: 270,
+            height: 50,
+            backgroundColor: "white",
+            marginTop: 20,
+            borderRadius: 12,
+            borderColor: "#9041fe",
+            paddingLeft: 10,
+            borderWidth: 1,
+          }}
+        />
+
+        <TextInput
+          placeholder="Confirm Password"
+          style={{
+            width: 270,
+            height: 50,
+            backgroundColor: "white",
+            marginTop: 20,
+            borderRadius: 12,
+            borderColor: "#9041fe",
+            paddingLeft: 10,
+            borderWidth: 1,
+          }}
+        />
+      </View>
+
+      <View
+        style={{
+          flex: 0.2,
+
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Pressable>
+          <LinearGradient
+            colors={["#854efe", "#28b6ff"]}
+            style={styles.gradient}
+          >
+            <Text style={styles.text} onPress={() => Alert.alert('Sign Up successfull')}>Sign Up</Text>
+          </LinearGradient>
+        </Pressable>
+      </View>
     </View>
   );
 }
@@ -17,24 +115,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#1E80E2',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  gradient: {
+    flex: 0.5,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 5,
+    width: 100,
   },
   button: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical: 12,
-    paddingHorizontal: 32,
-    borderRadius: 4,
-    elevation: 3,
-    backgroundColor: 'black',
+    width: 100,
+    height: 45,
   },
   text: {
+    color: "white",
     fontSize: 16,
-    lineHeight: 21,
-    fontWeight: 'bold',
-    letterSpacing: 0.25,
-    color: 'white',
   },
 });
